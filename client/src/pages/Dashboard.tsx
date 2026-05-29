c8e9d51751e8ba52d3994451a5fd6be0f59cb5a2
Priyansh <electroniorje@gmail.com>
2026-05-30 00:45:13 +0530

    Main Section

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
+import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 
 function Dashboard() {
     return <div className="h-screen bg-primary">
         <Navbar/>
+        <Main />
     </div>
 }
 
